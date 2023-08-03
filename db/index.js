42a8c7eac5fb54a3de054ba9960e234f97563754
Gino Rocca <roccagino5@gmail.com>
2023-08-03 12:23:53 +0200

    update index

--- db/index.js
+++ db/index.js
@@ -1,7 +1,6 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
-ssl = true;
+const pool = new Pool({ ssl: true });
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
